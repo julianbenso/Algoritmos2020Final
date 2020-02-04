2cc607e240b5e595a62c3cf599904e2c1513f7db
julian <julianbenso_97@hotmail.com>
2020-02-04 02:42:28 -0300

    Multiples cambios: Se cambiaron los nombres de las variables a otras mas intuitivas. Se agrego la funcion search_middle que es la encargada de buscar el valor medio del nodo. Se logro hacer que la funcion funcione cuando el nodo esta lleno y se ingresa un valor el cual es el medio del nodo. Se logro hacer que la funcion funcione cuando el nodo esta lleno y se ingresa un valor el cual es menor al medio del nodo. Falta hacer que funcione con grado de variable 3,4,5,6, funcionando alctualmente con grado 6. Hacer la funcion buscar_valor y altura_arbol. Hacer que el arbol permita una altura 3 o superior, que admita separar un nodo hijo, para llevar un valor al padre.

--- main.cpp
+++ main.cpp
@@ -6,142 +6,301 @@
 #include<iostream>
 using namespace std;
 int grado=6;
+int cantClaves= grado-1;
 struct BpTreeNode
 {
     int *data;
     BpTreeNode **child_ptr;
     bool leaf;
-    int n;
+    int n;//cantidad de lugares ocupados en el nodo
 }
-    *root = NULL, *np = NULL, *x = NULL;
+    *root = nullptr, *node = nullptr, *nodoRoot = nullptr;
 BpTreeNode * init()
 {
-    int i;
-    //int grado=5;
-    int numhijos= grado;
-    np = new BpTreeNode;
-    np->data = new int[grado-1];
-    np->child_ptr = new BpTreeNode *[numhijos];
-    np->leaf = true;
-    np->n = 0;
-    for (i = 0; i < 6; i++)
-    {
-        np->child_ptr[i] = NULL;
+    int numHijos = grado;
+    node = new BpTreeNode;
+    node->data = new int[cantClaves];
+    node->child_ptr = new BpTreeNode *[numHijos];
+    node->leaf = true;
+    node->n = 0;
+    for (int i = 0; i < grado; i++){
+        node->child_ptr[i] = nullptr;
     }
-    return np;
+    return node;
 }
-void traverse(BpTreeNode *p)
+void print_tree(BpTreeNode* p)
 {
-    cout<<endl;
     int i;
+    cout<<endl;
     for (i = 0; i < p->n; i++)
     {
         cout<<"  ";
         if (p->leaf == false)
         {
-            traverse(p->child_ptr[i]);
+            print_tree(p->child_ptr[i]);
         }
 
-        cout << "" << p->data[i];
+        cout << p->data[i];
     }
 
     if (p->leaf == false)
     {
-        traverse(p->child_ptr[i]);
+        print_tree(p->child_ptr[i]);
     }
     cout<<endl;
 }
-void sort(int *p, int n)
+
+void sort(int *node, int lugaresOcupados)
 {
     int i, j, temp;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < lugaresOcupados; i++)
     {
-        for (j = i; j <= n; j++)
+        for (j = i; j <= lugaresOcupados; j++)
         {
-            if (p[i] > p[j])
-            {
-                temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
+            if (node[i] > node[j]){
+                temp = node[i];
+                node[i] = node[j];
+                node[j] = temp;
             }
         }
     }
 }
-int split_child(BpTreeNode *x, int i, int a)
+
+int search_middle(BpTreeNode *nodo, int valIngresado, int *midpX, int *posMedio, int *separar){
+    // la variable "valIngresado" es el valor a ingresar en el nodo, que desencadena la separacion del nodo.
+    //si el grado de variable es 3 (2 claves).
+    /* la variable "midpX", indica a la funcion split_child si el valor medio estaba en nodo , o si es el
+     valor nuevo ingresado es el valor medio.
+     midpX= 1 indica que el elemento medio pertenece a nodo
+     midpX= -1 indica que el elemento medio NO pertenece a nodo
+     */
+    //la variable "posMedio" indica la posicion del valor medio en caso de este pertenecer a nodo.
+
+    int middle;
+    if (cantClaves==2)/*GRADO 3*/ {
+        if (valIngresado < nodo->data[0]){
+            middle = nodo->data[0];
+            *posMedio=0;
+            *midpX=1;
+            return middle;
+        }
+        if(nodo->data[0] < valIngresado && valIngresado < nodo->data[1]){
+            middle = valIngresado;
+            *separar= 0;
+            *midpX=-1;
+            return middle;
+        }
+        if(nodo->data[1] < valIngresado){
+            middle = nodo->data[1];
+            *posMedio=1;
+            *midpX=1;
+            return middle;
+        }
+    }
+    else if(cantClaves==3)/*GRADO 4*/{
+        if(valIngresado < nodo->data[0] || valIngresado < nodo->data[1]){
+            middle = nodo->data[1];
+            *posMedio=1;
+            *midpX=1;
+            return middle;
+        }
+        if(nodo->data[1] < valIngresado && valIngresado < nodo->data[2]){
+            middle = valIngresado;
+            *separar= 1;
+            *midpX=-1;
+            return middle;
+        }
+        if(nodo->data[2] < valIngresado){
+            middle = nodo->data[2];
+            *posMedio=2;
+            *midpX=1;
+            return middle;
+        }
+    }
+    else if (cantClaves==4)/*GRADO 5*/{
+        if(valIngresado < nodo->data[0] || valIngresado < nodo->data[1]){
+            middle = nodo->data[1];
+            *posMedio=1;
+            *midpX=1;
+            return middle;
+        }
+        if(nodo->data[1] < valIngresado && valIngresado < nodo->data[2]){
+            middle = valIngresado;
+            *separar= 1;
+            *midpX=-1;
+            return middle;
+        }
+        if(nodo->data[2] < valIngresado || nodo->data[3] < valIngresado) {
+            middle = nodo->data[2];
+            *posMedio=2;
+            *midpX=1;
+            return middle;
+        }
+    }
+    else if (cantClaves==5)/*GRADO 6*/{
+        if(valIngresado < nodo->data[0] || valIngresado < nodo->data[1] || valIngresado < nodo->data[2]){
+            middle = nodo->data[2];
+            *posMedio=2;
+            *midpX=1;
+            return middle;
+        }
+        if(nodo->data[2] < valIngresado && valIngresado < nodo->data[3]){
+            middle = valIngresado;
+            *separar= 3;
+            *midpX=-1;
+            return middle;
+        }
+        if(nodo->data[3] < valIngresado || nodo->data[4] < valIngresado) {
+            middle = nodo->data[3];
+            *posMedio=3;
+            *midpX=1;
+            return middle;
+        }
+    }
+}
+
+int split_child(BpTreeNode *nodoOriginal, int i, int valIngresado)
 {
     int j, mid;
-    BpTreeNode *np1, *np3, *y;
-    np3 = init();
-    np3->leaf = true;
-    int separarDesde=((grado-1)/2)+1;
-    if (i == -1)
-    {
-        mid = x->data[separarDesde];
-        x->data[separarDesde] = 0;
-        x->n--;
-        np1 = init();
-        np1->leaf = false;
-        x->leaf = true;
+    BpTreeNode *padre, *hijoMenor, *hijoMayor, *y;
+    int midpX=0;  //parametro numero 3 de search_middle
+    int posMedio=0;   //posicion del elemento medio
+    int separar=0;  //para separar desde un cierto elemento cuando el elemento medio no pertenece a nodoOriginal
+    int separarDesde = search_middle(nodoOriginal, valIngresado, &midpX, &posMedio, &separar);
 
-        for (j = separarDesde+1; j <5; j++)
-        {
-            np3->data[j - separarDesde-1] = x->data[j];
-            np3->child_ptr[j - separarDesde-1] = x->child_ptr[j];
-            np3->n++;
-            x->data[j] = 0;
-            x->n--;
+    if (i == -1) //si hay que separar un nodo, dado que esta lleno y es una hoja, sin padre
+    {
+        cout << "\nEl elemento medio es:" << search_middle(nodoOriginal, valIngresado, &midpX, &posMedio, &separar) << endl;
+        mid = search_middle(nodoOriginal, valIngresado, &midpX, &posMedio, &separar);
+
+        if (midpX == -1)/*si el elemento a insertar NO pertenece a NodoOriginal*/ {
+            cout << "El elemento medio NO pertenece a X";
+            padre = init();
+            padre->leaf = false;
+            hijoMenor = init();
+            hijoMenor->leaf = true;
+            hijoMayor = init();
+            hijoMayor->leaf = true;
+
+            //bucle para rellenar el hijo menor (hijo Izquierdo)
+            cout << endl << "El hijo menor es: ";
+            for (j = 0; j < separar; j++) {
+                hijoMenor->data[j] = nodoOriginal->data[j];
+                hijoMenor->child_ptr[j] = nodoOriginal->child_ptr[j];
+                hijoMenor->n++;
+                cout << " " << hijoMenor->data[j];
+            }
+            //bucle para rellenar el hijo mayor (hijo Derecho)
+            cout << endl << "El hijo mayor es: ";
+            for (j = separar; j < cantClaves; j++) {
+                hijoMayor->data[j - separar] = nodoOriginal->data[j];
+                hijoMayor->child_ptr[j - separar] = nodoOriginal->child_ptr[j];
+                hijoMayor->n++;
+                cout << " " << hijoMayor->data[j - separar];
+            }
+            cout << endl << endl;
+            padre->data[padre->n] = mid;
+            padre->child_ptr[padre->n] = hijoMenor;
+            padre->child_ptr[padre->n + 1] = hijoMayor;
+            padre->n++;
+            root = padre;
         }
-        for(j = 0; j < grado; j++)
+
+        if (midpX == 1) /*si el elemento a insertar pertenece a nodoOriginal*/
         {
-            x->child_ptr[j] = NULL;
+            cout << "El elemento medio pertenece a X\n";
+            padre = init();
+            padre->leaf = false;
+            hijoMenor = init();
+            hijoMenor->leaf = true;
+            hijoMayor = init();
+            hijoMayor->leaf = true;
+
+            //bucle para rellenar el hijo menor (hijo Izquierdo)
+            for (j = 0; j < posMedio; j++) {
+                hijoMenor->data[j] = nodoOriginal->data[j];
+                hijoMenor->child_ptr[j] = nodoOriginal->child_ptr[j];
+                hijoMenor->n++;
+            }
+            if(valIngresado < nodoOriginal->data[posMedio]){
+                hijoMenor->data[hijoMenor->n] = valIngresado;
+                sort(hijoMenor->data,hijoMenor->n);
+                hijoMenor->n++;
+            }
+            cout << endl << "El hijo menor es: ";
+            for(j=0;j < hijoMenor->n;j++){
+                cout << " " << hijoMenor->data[j];
+            }
+
+            //bucle para rellenar el hijo mayor (hijo Derecho)
+            for (j = posMedio+1; j < cantClaves; j++) {
+                hijoMayor->data[j - posMedio-1] = nodoOriginal->data[j];
+                hijoMayor->child_ptr[j - posMedio-1] = nodoOriginal->child_ptr[j];
+                hijoMayor->n++;
+            }
+            if(nodoOriginal->data[posMedio] < valIngresado){
+                hijoMayor->data[hijoMayor->n] = valIngresado;
+                sort(hijoMayor->data,hijoMayor->n);
+                hijoMayor->n++;
+            }
+            cout << endl << "El hijo mayor es: ";
+            for(j=0;j < hijoMayor->n;j++){
+                cout << " " << hijoMayor->data[j];
+            }
+            cout << endl << endl;
+
+            padre->data[padre->n] = mid;
+            padre->n++;
+            padre->child_ptr[padre->n] = hijoMenor;
+            padre->child_ptr[padre->n + 1] = hijoMayor;
+            root = padre;
         }
-        np1->data[0] = mid;
-        np1->child_ptr[np1->n] = x;
-        np1->child_ptr[np1->n + 1] = np3;
-        np1->n++;
-        root = np1;
     }
     else
     {
-        y = x->child_ptr[i];
+        /*FALTA HACERLA CORRECTAMENTE*/
+        hijoMenor=init();
+        y = nodoOriginal->child_ptr[i];
         mid = y->data[separarDesde];
         y->data[separarDesde] = 0;
         y->n--;
         for (j = separarDesde+1; j < 5; j++)
         {
-            np3->data[j - separarDesde-1] = y->data[j];
-            np3->n++;
+            hijoMenor->data[j - separarDesde-1] = y->data[j];
+            hijoMenor->n++;
             y->data[j] = 0;
             y->n--;
         }
-        x->child_ptr[i + 1] = y;
-        x->child_ptr[i + 1] = np3;
+        nodoOriginal->child_ptr[i + 1] = y;
+        nodoOriginal->child_ptr[i + 1] = hijoMenor;
     }
     return mid;
+
 }
-void insert(int a)
+void insert(int valIngresado)
 {
-    int i, temp;
-    x = root;
-    if (x == NULL)
+    int i, valorNuevoEnPadre;
+    nodoRoot = root;
+    if (nodoRoot == nullptr)
     {
         root = init();
-        x = root;
+        nodoRoot = root;
     }
     else
     {
-        if (x->leaf == true && x->n == 5)
+        if (nodoRoot->leaf == true && nodoRoot->n == 5)
         {
-            temp = split_child(x, -1,a);
-            x = root;
-            for (i = 0; i < (x->n); i++)
+
+            valorNuevoEnPadre = split_child(nodoRoot, -1, valIngresado);
+            nodoRoot = root;
+            for (i = 0; i < (nodoRoot->n); i++)
             {
-                if ((a > x->data[i]) && (a < x->data[i + 1]))
+                if ((nodoRoot->data[i] < valIngresado) && (valIngresado < nodoRoot->data[i + 1]))
                 {
                     i++;
                     break;
                 }
-                else if (a < x->data[0])
+                else if (valIngresado < nodoRoot->data[0])
                 {
                     break;
                 }
@@ -150,21 +309,22 @@ void insert(int a)
                     continue;
                 }
             }
-            x = x->child_ptr[i];
-            insert(temp);
+            nodoRoot = nodoRoot->child_ptr[i];
+            insert(valorNuevoEnPadre);
+            return;
         }
         else
         {
-            while (x->leaf == false)
+            while (nodoRoot->leaf == false)
             {
-                for (i = 0; i < (x->n); i++)
+                for (i = 0; i < (nodoRoot->n); i++)
                 {
-                    if ((a > x->data[i]) && (a < x->data[i + 1]))
+                    if ((valIngresado > nodoRoot->data[i]) && (valIngresado < nodoRoot->data[i + 1]))
                     {
                         i++;
                         break;
                     }
-                    else if (a < x->data[0])
+                    else if (valIngresado < nodoRoot->data[0])
                     {
                         break;
                     }
@@ -173,38 +333,39 @@ void insert(int a)
                         continue;
                     }
                 }
-                if ((x->child_ptr[i])->n == 5)
+                if ((nodoRoot->child_ptr[i])->n == 5)
                 {
-                    temp = split_child(x, i, a);
-                    x->data[x->n] = temp;
-                    x->n++;
-                    insert(temp);
+                    valorNuevoEnPadre = split_child(nodoRoot, 1, valIngresado);
+                    nodoRoot->data[nodoRoot->n] = valorNuevoEnPadre;
+                    nodoRoot->n++;
+                    insert(valorNuevoEnPadre);
                     continue;
                 }
                 else
                 {
-                    x = x->child_ptr[i];
+                    nodoRoot = nodoRoot->child_ptr[i];
 
                 }
             }
         }
     }
-    x->data[x->n] = a;
-    sort(x->data, x->n);
-    x->n++;
+    nodoRoot->data[nodoRoot->n] = valIngresado;
+    sort(nodoRoot->data, nodoRoot->n);
+    nodoRoot->n++;
 }
+
 int main()
 {
-    int i, n, t;
-    cout<<"enter the no of elements to be inserted\n";
-    cin>>n;
-    for(i = 0; i < n; i++)
+    int cantidadElementos, ValorIngresado;
+    cout<<"Ingresar cantidad de elementos a insertar: ";
+    cin>>cantidadElementos;
+    for(int i = 0; i < cantidadElementos; i++)
     {
-        cout<<"enter the element\n";
-        cin>>t;
-        insert(t);
+        cout << "Ingresar valor: ";
+        cin >> ValorIngresado;
+        insert(ValorIngresado);
     }
-    cout<<"traversal of constructed tree\n";
-    traverse(root);
+    cout << "Arbol trasversal:";
+    print_tree(root);
     //getch();
 }
